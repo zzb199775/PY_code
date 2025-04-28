da0c586fe2beb146d3cd805db0c1e9c8185b8bf3
张志博 <ch_zzb_19970705@163.com>
2025-04-28 21:38:44 +0800

    text

--- main.py
+++ main.py
@@ -4,3 +4,6 @@
 # @time               : 2025/4/26 22:13
 # @Project description: About learning and thinking about Python
 print("Hello,python")
+print("Hello,脏脏包")
+print("Hello,zhangzhibo")
+# print("Hello,python")
