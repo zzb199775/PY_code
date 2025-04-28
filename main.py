a38924716413e98023021c27a0bfc03f251f4826
张志博 <ch_zzb_19970705@163.com>
2025-04-28 21:19:28 +0800

    Initial commit

---
+++ main.py
@@ -0,0 +1,5 @@
+# @Version            : 1.0
+# @Author             : 张志博
+# @File               : main.py
+# @time               : 2025/4/26 22:13
+# @Project description: About learning and thinking about Python
